a0c7632501429e4c92273ba81338baab0c13a11a
agoncharov <gonas.hans@gmail.com>
2022-06-30 15:45:21 +0300

    Add Game state/mode/instanse, Player state/controller, Character, TestMap

---
+++ Source/Platformer/Public/Character/PlatCharacterBase.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "PlatCharacterBase.generated.h"
+
+UCLASS()
+class PLATFORMER_API APlatCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlatCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Camera")
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Camera")
+	UCameraComponent* FollowCamera;
+
+};
